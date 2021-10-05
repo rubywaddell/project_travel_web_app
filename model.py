8bac474b49b7b0559acb7e86886906c40d0f1240
Ruby Waddell <rubywaddell@gmail.com>
2021-10-05 10:47:54 -0700

    Complete users table set up in model.py

--- model.py
+++ model.py
@@ -17,4 +17,19 @@ class User(db.Model):
 
     def __repr__(self):
         return f"<User object: user_id={self.user_id}, username={self.username}, full_name={self.full_name}>"
-    
+
+
+def connect_to_db(flask_app, db_uri="postgresql:///travel_project", echo=True):
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+    flask_app.config["SQLALCHEMY_ECHO"] = echo
+    flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print("Connected to the db!")
+
+if __name__ == "__main__":
+    from server import app
+
+    connect_to_db(app)
