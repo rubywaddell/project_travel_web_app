1f708be6bf02c7bcee77e5c2e959fba1f2a963f5
Ruby Waddell <rubywaddell@gmail.com>
2021-10-06 19:57:28 -0700

    Write tests in test_seed.py to test for query funcionality and relation between tips and users

--- test_seed.py
+++ test_seed.py
@@ -84,6 +84,15 @@ class TestSeedDatabase(unittest.TestCase):
         test_length = 10
         self.assertEqual(test_length, len(seed_tips))
 
+    def test_tip_w_user_id_seeded_to_db(self):
+        """Test that tip objects with relationships to users are successfully seed to database"""
+
+        seed_tips = seed_database.tips_in_db
+        test_user_id = 3
+        test_tip = seed_tips[2]
+
+        self.assertEqual(test_user_id, test_tip.user_id)
+
     def test_tag_seeded_to_db(self):
         """Test that the tags table has successfully been seeded with data"""
 
@@ -113,6 +122,22 @@ class TestSeedDatabase(unittest.TestCase):
         test_tag_id = 1
         test_tip_tag= seed_tip_tags[0]
         self.assertEqual(test_tag_id, test_tip_tag.tag_id)
+
+    def test_tip_tag_to_tag_query(self):
+        """Test that query functionality between tip_tag and tag objects is working properly"""
+
+        test_tag = seed_database.tags_in_db[0]
+        test_tip_tag = seed_database.tip_tags_in_db[0]
+
+        self.assertEqual(test_tag.tag_name, test_tip_tag.tag.tag_name)
+
+    def test_tip_tag_to_tip_query(self):
+        """Test that query functionality between tip_tag and tip objects is working properly"""
+
+        test_tip = seed_database.tips_in_db[2]
+        test_tip_tag = seed_database.tip_tags_in_db[2]
+
+        self.assertEqual(test_tip.tip_text, test_tip_tag.tip.tip_text)
 
 if __name__ == "__main__":
     unittest.main()
