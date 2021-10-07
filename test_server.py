a62a72a001bd07cfed82d5092f93f4f3c9e485ce
Ruby Waddell <rubywaddell@gmail.com>
2021-10-07 12:06:20 -0700

    Write test to test that homepage includes a navigation list

--- test_server.py
+++ test_server.py
@@ -16,11 +16,17 @@ class TestHomePageRoutes(unittest.TestCase):
         self.client = server.app.test_client()
         server.app.config["TESTING"] = True
 
-    def test_home_page_route(self):
-        """Test that the home_page loads when / route is accessed"""
+    def test_homepage_route(self):
+        """Test that the homepage loads when / route is accessed"""
 
         result = self.client.get("/")
-        self.assertIn(b"Welcome", result.data)
+        self.assertIn(b"Welcome!", result.data)
+    
+    def test_homepage_navigation(self):
+        """Test that the homepage links to other pages in the site"""
+
+        result = self.client.get("/")
+        self.assertIn(b"<ul>Navigation:</ul>", result.data)
 
 if __name__ == "__main__":
     unittest.main()
