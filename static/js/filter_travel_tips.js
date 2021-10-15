112d3626f83ff2845ae89ca1b39f68729c43e549
Ruby Waddell <rubywaddell@gmail.com>
2021-10-14 17:04:46 -0700

    Write initial AJAX travel tips filtering function, if location tag selected, new inputs appear to input state and city

--- static/js/filter_travel_tips.js
+++ static/js/filter_travel_tips.js
@@ -1,10 +1,21 @@
 "use strict";
+console.log('connected to js')
 
 const filterTips = (evt) => {
     evt.preventDefault();
-    alert("Prevent default is working")
+    // alert("Prevent default is working")
 
+    const tag_val = $('input[name="filter-tags"]:checked').val()
+
+    if (tag_val === 'location'){
+        console.log(true)
+        $('#travel-tips-filter-tags').html(`<label>Select the Location You'd Like to Filter:</label><p></p>
+                    <label for="state">State: </label>
+                    <input id="search-destination-state-input" type="text" name="state">
+                    <label for="city"> City: </label>
+                    <input id="search-destination-city-input" type="text" name="city">`)
+    }
 };
 
 
-$('#travel-tips-filter-btn').on('click', filterTips);
+$('#travel-tips-filter-submit').on('click', filterTips);
