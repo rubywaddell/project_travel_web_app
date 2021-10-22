1cfa2a9e57f292dc8923f09271cbe5d2d051fac8
Ruby Waddell <rubywaddell@gmail.com>
2021-10-22 10:21:18 -0700

    Add functionality to show the change username form when user clicks on update username button

--- static/js/edit_profile.js
+++ static/js/edit_profile.js
@@ -18,7 +18,19 @@ const showEditEmailForm = () => {
 }
 
 const showEditUsernameForm = () => {
-    alert('showEditUsernameForm function on click is working');
+    $('#edit-username-paragraph').html(`
+    <div id="edit-user-username">
+    <form id="edit-user-username" action="/edit_username">
+        <label><strong>Change Your Username:</strong></label>
+        <p></p>
+        <label>Old username:</label>
+        <input type="text" name="old-username">
+        <label>New username:</label>
+        <input type="text" name="new-username" pattern="\w{1, 15}" title="Username too long, please limit to 15 characters">
+        <input type="submit" id="change-username-submit">
+    </div>
+    </form>
+    `);
 }
 
 const showEditPasswordForm = () => {
