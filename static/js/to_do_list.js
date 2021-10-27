183f3dfae6dbd67094a53e55e13c6eabb3de6117
Ruby Waddell <rubywaddell@gmail.com>
2021-10-26 18:02:37 -0700

    Add test button functionality to make sure on click event listeners are working

--- static/js/to_do_list.js
+++ static/js/to_do_list.js
@@ -1,3 +1,29 @@
 "use strict";
 
-alert("JS connected")
+// Prep list functions
+const addPrepListItem = () => {
+    alert('You sure clicked that button');
+}
+
+$('#prep-list-add-item-btn').on('click', addPrepListItem);
+
+// Clothing packing list functions
+const addClothesListItem = () => {
+    alert('You sure clicked that button');
+}
+
+$('#clothes-list-add-item-btn').on('click', addClothesListItem);
+
+// Toiletries packing list functions
+const addToiletriesListItem = () => {
+    alert('You sure clicked that button');
+}
+
+$('#toiletries-list-add-item-btn').on('click', addToiletriesListItem);
+
+// Misc. packing lsit functions
+const addMiscListItem = () => {
+    alert('You sure clicked that button');
+}
+
+$('#misc-list-add-item-btn').on('click', addMiscListItem)
