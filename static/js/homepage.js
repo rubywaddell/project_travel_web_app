2b0bb4fa4ab99297a4763e5d3769554602dae74a
Ruby Waddell <rubywaddell@gmail.com>
2021-11-01 15:54:13 -0700

    Add images and videos to git, update homepage video styling

--- static/js/homepage.js
+++ static/js/homepage.js
@@ -15,3 +15,26 @@ $.get("/check_session", response => {
         `);
     }
 });
+
+// ----------------------- FUNCTIONS FOR HOMEPAGE VIDEO -----------------------------
+const homepageVideo = document.querySelector('.background-video video');
+
+const scaleVideo = () => {
+    const innderWidth = window.innerWidth;
+    const innerHeight = window.innerHeight;
+
+    if (innderWidth > innerHeight){
+        homepageVideo.setAttribute('width', innerWidth);
+        homepageVideo.setAttribute('height', '');
+    }else{
+        homepageVideo.setAttribute('width', '');
+        homepageVideo.setAttribute('height', innerHeight);
+    }
+
+    // const videoHeight = getComputedStyle(homepageVideo).height;
+    // document.querySelector('header').style.height = videoHeight;
+}
+
+window.onload = scaleVideo;
+
+window.onresize = scaleVideo;
