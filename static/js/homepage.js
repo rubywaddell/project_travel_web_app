2e1b744cc60ce199870455fbd99bc1feab28f686
Ruby Waddell <rubywaddell@gmail.com>
2021-11-02 14:48:54 -0700

    Change login/logout card to button on homepage

--- static/js/homepage.js
+++ static/js/homepage.js
@@ -4,18 +4,23 @@ $.get("/check_session", response => {
     if (response === "False"){
         console.log("Not logged in");
     }else{
+        // change homepage card from displaying create account info, to
+        // display profile info and link to profile in button
         $('#homepage-account-paragraph').html(`
         View your profile to add tips, add new trips, and plan for your 
         trips with our personalizable packing lists
-            <button class="btn btn-outline-primary"><a id="homepage-profile-link" 
-                href="/profile_${response}">View Profile</a>
-            </button>
         `);
+        $('#homepage-account-link').html(`
+            <a id="homepage-profile-link" 
+                href="/profile_${response}">View Profile</a>
+        `)
+        // change homepage button from link to log in, to link to log out
         $('#homepage-login-paragraph').html(`
-            <button class="btn btn-outline-primary">
+            <button class="btn btn-primary">
                 <a id="homepage-login-link" href="/logout">Log Out</a>
             </button>
         `);
+
     }
 });
 
